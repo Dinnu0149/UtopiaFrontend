44a2c4882e7553d1a17e85c0b53a64c0a63cb8c7
Adinnu Somtochukwu <somtodaniel12@gmail.com>
2024-12-30 16:37:39 +0100

    HashRouter

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "../pages/Authentication/SignUp";
 import Login from "../pages/Authentication/Login";
 import ForgetPassword from "../pages/Authentication/ForgetPassword";
